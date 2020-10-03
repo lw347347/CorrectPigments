3deae93df79d8993c5c6b396a9b35214f449d4d9
unknown <lw347347@byu.edu>
2020-10-02 22:29:15 -0600

    Fixes

--- WebSockets/WebSockets/asgi.py
+++ WebSockets/WebSockets/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_default_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebSockets.settings')
+
+django.setup()
 
 application = get_default_application()
